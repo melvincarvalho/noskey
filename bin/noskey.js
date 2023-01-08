3336b4fa2afef3aeb7fedc9236be78e3531871c6
Melvin Carvalho <https://melvincarvalho.com/#me>
2023-01-08 19:37:44 +0100

    lint and add bitcoin public key address

--- bin/noskey.js
+++ bin/noskey.js
@@ -1,19 +1,16 @@
 #!/usr/bin/env node
 
 // IMPORTS
-const {
-	generatePrivateKey,
-	getPublicKey,
-	nip19
-} = require("nostr-tools");
+const { generatePrivateKey, getPublicKey, nip19 } = require('nostr-tools')
 const {
 	encodeBytes,
 	generate_public_key,
 	hexToBase64,
 	bufferToHex
-} = require('../lib/index.js');
+} = require('../lib/index.js')
 const tiny = require('tiny-secp256k1')
-
+const bitcoin = require('bitcoinjs-lib')
+const { ECPairFactory } = require('ecpair')
 
 // args
 const argv = require('yargs')
@@ -29,12 +26,12 @@ const argv = require('yargs')
 		type: 'string'
 	})
 	.help('h')
-	.alias('h', 'help')
-	.argv;
+	.alias('h', 'help').argv
 
 // INIT
 // console.log(argv)
 var vanity = argv.v || ''
+const ECPair = ECPairFactory(tiny)
 
 const ed25519_prefix = '0000000b7373682d6564323535313900000020'
 const ed25519_ssh_prefix = 'ssh-ed25519'
@@ -44,9 +41,13 @@ const taproot_testnet_prefix = 'tb1p'
 // MAIN
 while (true) {
 	var privateKey = argv.p || generatePrivateKey()
-	let publicKey = getPublicKey(privateKey);
+	let publicKey = getPublicKey(privateKey)
 	let npub = nip19.npubEncode(publicKey)
 	// console.log(npub)
+
+	const keyPair = ECPair.fromPrivateKey(Buffer.from(privateKey, 'hex'))
+	let wif = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey })
+	// console.log(keyPair.publicKey)
 
 	if (publicKey.startsWith(vanity)) {
 		var ed25519pubkey = generate_public_key(privateKey)
@@ -58,21 +59,18 @@ while (true) {
 			nsec: nip19.nsecEncode(privateKey),
 			pubkey: publicKey,
 			pubkeycompressed: compressed,
+			bitcoinPubkey: wif.address,
 			npub: npub,
 			taproot: encodeBytes(taproot_prefix, publicKey),
 			taproottestnet: encodeBytes(taproot_testnet_prefix, publicKey),
 			ed25519pubkey: ed25519pubkey,
-			openSSHed25519pubkey: `${ed25519_ssh_prefix} ${hexToBase64(ed25519_prefix + ed25519pubkey)}`
+			openSSHed25519pubkey: `${ed25519_ssh_prefix} ${hexToBase64(
+				ed25519_prefix + ed25519pubkey
+			)}`
 		}
 
 		console.log(JSON.stringify(output, null, 2))
 
-		break;
+		break
 	}
 }
-
-
-
-
-
-
