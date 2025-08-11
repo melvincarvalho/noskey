eaf7bfd0bafba5a7f316e5b358c0a8343db2d1eb
Melvin Carvalho <melvincarvalho@gmail.com>
2025-08-11 16:09:53 +0200

    cjs

--- bin/noskey.js
+++ bin/noskey.js
@@ -1,92 +1,82 @@
 #!/usr/bin/env node
+// Force explicit stdout flushing
+process.stdout.write('noskey starting...\n')
 
-async function main() {
-	console.log('noskey starting...')
+// IMPORTS
+import { generatePrivateKey } from 'nostr-tools'
+import {
+	getAllKeys,
+	decodeBytes
+} from '../lib/index.js'
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
 
-	// IMPORTS
-	try {
-		var { generatePrivateKey } = await import('nostr-tools')
-		var { getAllKeys, decodeBytes } = await import('../lib/index.js')
-		var yargs = (await import('yargs')).default
-		var { hideBin } = await import('yargs/helpers')
-		console.log('All imports successful')
-	} catch (error) {
-		console.error('Import error:', error)
-		process.exit(1)
-	}
+console.log('noskey: imports loaded')
 
-	// args
-	const yarg = yargs(hideBin(process.argv))
+// args
+const yarg = yargs(hideBin(process.argv))
 
-	// console.log(yargs)
-	const argv = yarg
-		.usage('Usage: $0 [options]')
-		.option('v', {
-			alias: 'vanity',
-			describe: 'Vanity string',
-			type: 'string'
-		})
-		.option('n', {
-			alias: 'npub',
-			describe: 'npub Vanity string',
-			type: 'string'
-		})
-		.option('p', {
-			alias: 'priv',
-			describe: 'Private key',
-			type: 'string'
-		})
-		.option('s', {
-			alias: 'nsec',
-			describe: 'nsec private key',
-			type: 'string'
-		})
-		.help('h')
-		.alias('h', 'help').argv
+// console.log(yargs)
+const argv = yarg
+	.usage('Usage: $0 [options]')
+	.option('v', {
+		alias: 'vanity',
+		describe: 'Vanity string',
+		type: 'string'
+	})
+	.option('n', {
+		alias: 'npub',
+		describe: 'npub Vanity string',
+		type: 'string'
+	})
+	.option('p', {
+		alias: 'priv',
+		describe: 'Private key',
+		type: 'string'
+	})
+	.option('s', {
+		alias: 'nsec',
+		describe: 'nsec private key',
+		type: 'string'
+	})
+	.help('h')
+	.alias('h', 'help').argv
 
-	// INIT
-	// console.log(argv)
-	const vanity = argv.v || ''
-	const npubvanity = argv.n || ''
-	const nsec = argv.s
+console.log('noskey: args parsed')
 
-	// If no specific key provided and no vanity requirements, just generate one key
-	if (!nsec && !argv.p && !vanity && !npubvanity) {
-		console.log('Generating single key...')
-		try {
-			const privateKey = generatePrivateKey()
-			console.log('Private key generated:', privateKey.slice(0, 8) + '...')
-			const output = getAllKeys(privateKey)
-			console.log('Keys processed, outputting...')
-			console.log(JSON.stringify(output, null, 2))
-			console.log('Done!')
-		} catch (error) {
-			console.error('Error generating key:', error)
-		}
-		process.exit(0)
-	}
+// INIT
+// console.log(argv)
+const vanity = argv.v || ''
+const npubvanity = argv.n || ''
+const nsec = argv.s
 
-	// MAIN - for vanity generation or specific key processing
-	while (true) {
-		var privateKey
-		if (nsec) {
-			privateKey = decodeBytes(nsec)
-		} else if (argv.p) {
-			privateKey = argv.p
-		} else {
-			privateKey = generatePrivateKey()
-		}
-		const output = getAllKeys(privateKey)
-
-		const npub_prefix = 'npub1'
-		if (output.pubkey.startsWith(vanity) && output.npub.slice(npub_prefix.length).startsWith(npubvanity)) {
-			console.log(JSON.stringify(output, null, 2))
-			break
-		}
-	}
+// If no specific key provided and no vanity requirements, just generate one key
+if (!nsec && !argv.p && !vanity && !npubvanity) {
+	console.log('noskey: generating single key')
+	const privateKey = generatePrivateKey()
+	console.log('noskey: calling getAllKeys')
+	const output = getAllKeys(privateKey)
+	console.log(JSON.stringify(output, null, 2))
+	process.exit(0)
 }
 
-main().catch(error => {
-	console.error('Fatal error:', error)
-	process.exit(1)
-})
+console.log('noskey: entering vanity loop')
+
+// MAIN - for vanity generation or specific key processing
+while (true) {
+	var privateKey
+	if (nsec) {
+		privateKey = decodeBytes(nsec)
+	} else if (argv.p) {
+		privateKey = argv.p
+	} else {
+		privateKey = generatePrivateKey()
+	}
+	const output = getAllKeys(privateKey)
+
+	const npub_prefix = 'npub1'
+	if (output.pubkey.startsWith(vanity) && output.npub.slice(npub_prefix.length).startsWith(npubvanity)) {
+		console.log(JSON.stringify(output, null, 2))
+		break
+	}
+}
